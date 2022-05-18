a4c30e733af356375272ffac0c810f5656436ab7
Zachwebdev <105175181+Zachwebdev@users.noreply.github.com>
2022-05-18 18:44:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () => { selectElement('header').classList.toggle('active'); });
